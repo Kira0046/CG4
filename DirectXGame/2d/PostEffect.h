83fefeeafe1e7bc01e7e1b02f42b05c47fdb5dec
YAMATO <arkenjel@outlook.jp>
2023-06-29 12:31:28 +0900

    3-3

--- DirectXGame/2d/PostEffect.h
+++ DirectXGame/2d/PostEffect.h
@@ -10,9 +10,17 @@ public:
 	PostEffect();
 
 	/// <summary>
+	/// 初期化
+	/// </summary>
+	void Initialize();
+
+	/// <summary>
 	/// 描画コマンドの発行
 	/// </summary>
 	/// <param name="cmdList"></param>
 	void Draw(ID3D12GraphicsCommandList* cmdList);
+private:
+	ComPtr<ID3D12Resource> texBuff;
+	ComPtr<ID3D12DescriptorHeap> descHeapSRV;
 };
 
