1fe0fc243bd62c45e7d3fd61f24865e84ab94ce4
YAMATO <arkenjel@outlook.jp>
2023-07-12 15:02:06 +0900

    Revert "3-5"
    This reverts commit ded7b29cafd8efc30c2aa1e3ff697f5edc3bb615.

--- DirectXGame/2d/PostEffect.h
+++ DirectXGame/2d/PostEffect.h
@@ -1,10 +1,5 @@
 #pragma once
-#include <Windows.h>
-#include <wrl.h>
-#include <d3d12.h>
-#include <DirectXMath.h>
 #include "Sprite.h"
-
 class PostEffect :
 	public Sprite
 {
@@ -36,22 +31,14 @@ public:
 	/// </summary>
 	/// <param name="cmdList"></param>
 	void PostDrawScene(ID3D12GraphicsCommandList* cmdList);
-
-	/// <summary>
-	/// パイプライン生成
-	/// </summary>
-	void CreateGraphicPipelineState();
 private://静的メンバ変数
 	static const float clearColor[4];
 private:
-	Microsoft::WRL::ComPtr<ID3D12Resource> texBuff;
-	Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> descHeapSRV;
+	ComPtr<ID3D12Resource> texBuff;
+	ComPtr<ID3D12DescriptorHeap> descHeapSRV;
 
-	Microsoft::WRL::ComPtr<ID3D12Resource> depthBuff;
-	Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> descHeapRTV;
-	Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> descHeapDSV;
-
-	Microsoft::WRL::ComPtr<ID3D12PipelineState> pipelineState;
-	Microsoft::WRL::ComPtr<ID3D12RootSignature> rootSignature;
+	ComPtr<ID3D12Resource> depthBuff;
+	ComPtr<ID3D12DescriptorHeap> descHeapRTV;
+	ComPtr<ID3D12DescriptorHeap> descHeapDSV;
 };
 
